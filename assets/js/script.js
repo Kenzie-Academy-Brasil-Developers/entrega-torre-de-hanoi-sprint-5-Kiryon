121eb1a118b22e7c5ba0af4e48d51cbfe83788fd
fernandofrantz <fernandofrantz12@gmail.com>
2021-08-03 15:39:18 -0300

    Torres E Barras geradas com sucesso, alguns problemas refentes ao draggable na parte do drop, css implementado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,12 @@
 function addTowers() {
     let ids = ["first-tower", "second-tower", "third-tower"] 
-   
     for (let counter = 0; counter < ids.length; counter++){
 
     function adcElemento () {
-
         let divNova = document.createElement("div");
         divNova.setAttribute("id", ids[counter]);
 
         let conteudoNovo = document.createTextNode("Olá!");
-
-
         divNova.appendChild(conteudoNovo); 
     
         let divAtual = document.getElementById("area-do-jogo");
@@ -18,6 +14,79 @@ function addTowers() {
         }
         adcElemento();
     }
-    
 }
 addTowers();
+
+function addDiscs() {
+    let ids = ["first-disc", "second-disc", "third-disc"] 
+    for (let counter = 0; counter < ids.length; counter++){
+
+    function adcElemento () {
+        let divNova = document.createElement("div");
+        divNova.setAttribute("id", ids[counter]);
+        divNova.setAttribute("draggable", true);
+        divNova.setAttribute("class", "bolacha");
+
+        let conteudoNovo = document.createTextNode("bolacha!");
+        divNova.appendChild(conteudoNovo); 
+    
+        let divAtual = document.getElementById("area-do-jogo");
+        divAtual.appendChild(divNova);
+        }
+        adcElemento();
+    }
+}
+addDiscs();
+
+
+
+let discReplace = document.querySelectorAll(".bolacha");
+
+let dragStart = function(event){
+    // console.log("dragstart")
+}
+
+
+let dragEnd = function(event){
+    console.log("dragend");
+
+    discReplace.appendChild(document.getElementById(event.target.id));
+
+
+    // let novaAlocação = document.getElementById(event.target.id);
+    // discReplace.appendChild(novaAlocação);
+    
+
+
+}
+
+
+document.addEventListener("dragstart", dragStart);
+document.addEventListener("dragend", dragEnd);
+
+
+
+
+
+
+
+
+// function addDisc(){
+
+//     let ids = ["first-disc", "first-disc", "first-disc"];
+//     for (let counter = 0; counter < ids.length; ids++){
+
+//         function adcDiscos() {
+//             let divNova = document.createElement("div");
+//             divNova.setAttribute("id", ids[counter]);
+
+//             let conteudoNovo = document.createTextNode("bolacha");
+//             divNova.appendChild(conteudoNovo);
+
+//             let divAtual = document.getElementById("area-do-jogo");
+//             divAtual.appendChild(divNova);
+//         }
+//         adcDiscos();
+//     }
+// }
+// addDisc();
