311acfcb08720ba17c2bc95aea566006502e4808
Ivan Rowlands de Macedo <ivan.jump@globo.com>
2021-08-03 19:30:57 -0300

    mudanças no js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,29 @@
 let torre = [[3,2,1],[],[]];
 let position = ['p0','p1','p2','p3','t1','t2','t3']
-function animacao(){
+function initPos(){
     torre.forEach((torre, torreid) => {
         torre.forEach((barra, position)=> {
+            let r = document.querySelector('.d'+barra)
+            r.classList.remove('t'+(torreid))
+            r.classList.remove('p'+(position))
             let d = document.querySelector('.d'+barra)
-            // position.forEach(position =>{
-            //     d.classList.remove(position)
-            // })
-            d.classList.remove(position);
             d.classList.add('t'+(torreid+1));
             d.classList.add('p'+(position+1));
         })
     })
 }
 function mover(datorre, paratorre){
+
+    if(!torre[datorre].length)return
     let barra = torre[datorre].pop();
+    if (torre[datorre].length){
+        if (torre[datorre][torre[paratorre].length-1]<barra){
+            return
+        }
+    }
     let d = document.querySelector('.d'+barra)
     d.classList.add('p0');
     torre[paratorre].push(barra);
-    setTimeout(animacao,2000);
+    setTimeout(initPos,2000);
 }
-animacao();
+initPos();
