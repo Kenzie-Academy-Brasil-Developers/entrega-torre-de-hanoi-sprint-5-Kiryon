72e2c9be33265130ac557c4fdb0efd36b132d794
Ivan Rowlands de Macedo <ivan.jump@globo.com>
2021-08-03 15:42:25 -0300

    commitando teste de nova versão

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+let torre = [[3,2,1],[],[]];
+let position = ['p0','p1','p2','p3','t1','t2','t3']
+function animacao(){
+    torre.forEach((torre, torreid) => {
+        torre.forEach((barra, position)=> {
+            let d = document.querySelector('.d'+barra)
+            // position.forEach(position =>{
+            //     d.classList.remove(position)
+            // })
+            d.classList.remove(position);
+            d.classList.add('t'+(torreid+1));
+            d.classList.add('p'+(position+1));
+        })
+    })
+}
+function mover(datorre, paratorre){
+    let barra = torre[datorre].pop();
+    let d = document.querySelector('.d'+barra)
+    d.classList.add('p0');
+    torre[paratorre].push(barra);
+    setTimeout(animacao,2000);
+}
+animacao();
